dc7f24a809d9e416f5b7f087dc337c583929b2ed
Matvey Khokhlov <matvey.khokhlov@epfl.ch>
2024-12-19 16:45:41 +0100

    Add the robot select screen

--- apps/thymio-suite-mobile-android/src/RobotSelect.tsx
+++ apps/thymio-suite-mobile-android/src/RobotSelect.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Button, Dimensions, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Dimensions, FlatList, Image, Linking, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 import { ConnectedRobot, RobotService } from "./services/RobotService";
+import { RobotButton } from "./components/RobotButton";
 import { NodeStatus } from "@mobsya-association/thymio-api/dist/thymio_generated/mobsya/fb";
+import { useLanguage } from "./i18n";
 
-function RobotSelect({ navigation }: any): JSX.Element {
+function RobotSelect(props: any): JSX.Element {
+  const { programmingEnv } = props.route.params;
+
+  const { i18n } = useLanguage();
+
   const robotService = new RobotService();
 
   const [robots, setRobots] = useState<ConnectedRobot[]>([]);
 
   useEffect(() => {
-    console.log('effect activated')
     robotService.scan();
     const robotSub = robotService.connectedRobots$.subscribe(robots => {
       console.log(robots.map(robot => robot.node.name))
@@ -22,36 +27,103 @@ function RobotSelect({ navigation }: any): JSX.Element {
     }
   }, [])
 
-  const robotList = robots.map(robot =>
-    <View key={robot.node.id.toString()}>
-      <Text>{robot.node.id.toString()}: {robot.node.statusAsString}</Text>
-      <Button
-        title={robot.node.name}
-        disabled={robot.node.status === NodeStatus.busy}
-        onPress={() => navigation.navigate('NewVPL3', {
-          name: robot.node.name,
-          uuid: robot.node.id.toString(),
-          host: robot.host,
-          port: robot.port
-        })}
-        ></Button>
-    </View>
+  const robotButtonList = robots.map(robot =>
+    <RobotButton
+      key={robot.node.id.toString()}
+      name={robot.node.name}
+      disabled={robot.node.status === NodeStatus.busy}
+      onPress={() => props.navigation.navigate(programmingEnv, {
+        name: robot.node.name,
+        uuid: robot.node.id.toString(),
+        host: robot.host,
+        address: robot.address,
+        port: robot.port
+      })}
+    ></RobotButton>
   );
 
   return (
     <SafeAreaView>
       <StatusBar />
       <View style={styles.rootContainer}>
-        <View style={styles.orange}>
-          <Text style={styles.title}>
-            LOREM IPSUM
-          </Text>
-          <Text>
-          Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-          </Text>
+        <View style={styles.descriptionContainer}>
+          <View style={styles.imageContainer}>
+            {programmingEnv === 'VPL3' ?
+              <Image source={require('./assets/vpl3-description.jpeg')} style={styles.image} /> :
+              <Image source={require('./assets/scratch-description.jpeg')} style={styles.image} />
+            }
+          </View>
+          <View style={styles.descriptionPadding}>
+            <Text style={styles.title}>
+              {programmingEnv === 'VPL3' ?
+                i18n.t('vpl3_description_title') :
+                i18n.t('scratch_description_title')
+              }
+            </Text>
+            <View
+              style={{
+                margin: 15,
+                borderBottomColor: 'white',
+                borderBottomWidth: StyleSheet.hairlineWidth,
+              }}
+            />
+            <View style={styles.descriptionText}>
+              <FlatList
+                data={programmingEnv === 'VPL3' ?
+                  [
+                    {key: i18n.t('vpl3_description_bullet_1')},
+                    {key: i18n.t('vpl3_description_bullet_2')},
+                    {key: i18n.t('vpl3_description_bullet_3')},
+                    {key: i18n.t('vpl3_description_bullet_4')}
+                  ]
+                  :
+                  [
+                    {key: i18n.t('scratch_description_bullet_1')},
+                    {key: i18n.t('scratch_description_bullet_2')},
+                    {key: i18n.t('scratch_description_bullet_3')},
+                    {key: i18n.t('scratch_description_bullet_4')}
+                  ]
+                }
+                renderItem={({item}) => {
+                  return (
+                    <View style={{ marginBottom: 10 }}>
+                      <Text style={{ fontSize: 15, color: 'white' }}>{`\u2022 ${item.key}`}</Text>
+                    </View>
+                  )
+                }}
+              />
+              <Text>{"\n"}</Text>
+              <Text
+                style={{color: 'lightblue'}}
+                onPress={
+                  () => {
+                    programmingEnv ?
+                      Linking.openURL('https://www.thymio.org/products/programming-with-thymio-suite/program-thymio-vpl3/') :
+                      Linking.openURL('https://www.thymio.org/products/programming-with-thymio-suite/program-thymio-scratch/')
+                  }
+                }>
+                  {i18n.t('description_help_and_tips')}
+              </Text>
+              <Text>{"\n"}</Text>
+              <Text style={{color: 'white'}}>
+                {i18n.t('description_materials')}
+                <Text
+                  style={{color: 'lightblue'}}
+                  onPress={() => Linking.openURL('https://www.thymio.org')}>
+                    https://www.thymio.org
+                </Text>
+              </Text>
+            </View>
+          </View>
         </View>
-        <View style={styles.blue}>
-          {robotList}
+        <View style={styles.robotContainer}>
+          <View style={styles.scannerContainer}>
+            <ActivityIndicator size='large' />
+            <Text style={styles.title}>{i18n.t('searching_robots')}</Text>
+          </View>
+          <View style={styles.robotList}>
+            {robotButtonList}
+          </View>
         </View>
       </View>
     </SafeAreaView>
@@ -62,22 +134,62 @@ export default RobotSelect;
 
 const styles = StyleSheet.create({
   rootContainer: {
+    backgroundColor: '#271845',
     display: 'flex',
     flexDirection: 'row',
     height: Dimensions.get('screen').height,
     width: Dimensions.get('screen').width
   },
-  orange: {
+  descriptionContainer: {
     flex: 1,
     color: 'white',
-    backgroundColor: 'orange'
   },
-  blue: {
+  imageContainer: {
+    width: Dimensions.get('screen').width / 2,
+    height: Dimensions.get('screen').height / 3
+  },
+  image: {
     flex: 1,
-    backgroundColor: 'blue'
+    width: undefined,
+    height: undefined,
+    resizeMode: 'cover'
+  },
+  descriptionPadding: {
+    padding: 15
   },
   title: {
     fontWeight: 'bold',
-    fontSize: 30
+    fontSize: 20,
+    color: 'white'
+  },
+  descriptionText: {
+    color: 'white',
+    padding: 20
+  },
+  robotContainer: {
+    padding: 20,
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20
+  },
+  scannerContainer: {
+    paddingLeft: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 20
+  },
+  robotList: {
+    padding: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    gap: 40
   }
 })
